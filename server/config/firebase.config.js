62ec9cf3d9e2ad605dfc8aaefea2cbf146389d20
Matin Imam <84035118+matin676@users.noreply.github.com>
2024-04-03 17:02:34 +0530

    Update firebase.config.js

--- server/config/firebase.config.js
+++ server/config/firebase.config.js
@@ -1,7 +1,7 @@
-console.log(__dirname)
+const path = require("path");
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
