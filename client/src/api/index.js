f4d1f16cc8b35876ef7e58ae5d9fe9170f136cce
Matin <matinimam77@gmail.com>
2024-04-04 14:23:13 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:4000/";
-const baseURL = "https://music-app-zga8.onrender.com";
+const baseURL = "https://music-app-zga8.onrender.com/";
 
 export const validateUser = async (token) => {
   try {
